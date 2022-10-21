750eab151d5994ca1f21bba37fd89daeed500082
FakeCore <flowunlight@gmail.com>
2022-10-21 13:08:53 +0800

    move struct_copy to struct_operation

--- utils/struct_operation_test.go
+++ utils/struct_operation_test.go
@@ -31,3 +31,23 @@ func TestOperation(t *testing.T) {
 		fmt.Println(err.Error())
 	}
 }
+
+type SrcData struct {
+	v1   int
+	Test string
+}
+type DestData struct {
+	Uk   int
+	v1   int
+	Test string
+}
+
+func TestStructCopy(t *testing.T) {
+	s := SrcData{v1: 23, Test: "hhhhh"}
+	var d DestData
+	StructCopy(&s, &d)
+	fmt.Printf("%v %v", s, d)
+	if s.v1 == d.v1 || s.Test != d.Test {
+		t.Error("copy failed")
+	}
+}
